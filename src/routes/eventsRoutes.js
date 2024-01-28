bdabe4d34705c3d4cf21ab6f76b3536aa5da261d
Kapil Kandel <kapil@evolveasia.co>
2024-01-28 14:15:21 +0545

    event routes updated

---
+++ src/routes/eventsRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import * as eventController from "../controllers/eventsController.js";
+import { verifyJWT } from "../middlewares/verifyJwt.js";
+
+
+const router = express.Router();
+
+router.route("/").get(eventController.eventsList);
+
+router.use(verifyJWT);
+
+router.route("/:id").get(eventController.getEventById);
+router.route("/").post(eventController.createEvent);
+router.route("/:id").patch(eventController.updateUserById);
+router.route("/:id").delete(eventController.deleteEventById);
+router.route("/:id/register").post(eventController.registerEvent);
+router.route("/:id/registrations").get(eventController.registrationList);
+
+export default router;
